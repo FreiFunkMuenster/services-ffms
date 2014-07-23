ad7e3c36275400f3f7f63530e146f7b0ce63dbb2
Thomas Hollstegge <thomas.hollstegge@zweitag.de>
2014-07-23 22:32:01 +0200

    Generierung der Indexseite hinzugefügt

---
+++ generate_overview/generate_overview.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+FIRMWARE_DIR=/var/www/firmware
+TEMPLATE_DIR=$(dirname $0)
+
+STABLE_VERSION=$(awk '{print $2}' < $FIRMWARE_DIR/stable/sysupgrade/manifest | sort | uniq | tail -n1)
+BETA_VERSION=$(awk '{print $2}' < $FIRMWARE_DIR/beta/sysupgrade/manifest | sort | uniq | tail -n1)
+EXPERIMENTAL_VERSION=$(awk '{print $2}' < $FIRMWARE_DIR/experimental/sysupgrade/manifest | sort | uniq | tail -n1)
+export STABLE_VERSION;
+export BETA_VERSION;
+export EXPERIMENTAL_VERSION;
+
+sed -e "s/STABLE_VERSION/$STABLE_VERSION/g" -e "s/BETA_VERSION/$BETA_VERSION/g" -e "s/EXPERIMENTAL_VERSION/$EXPERIMENTAL_VERSION/g" $TEMPLATE_DIR/template.html > $FIRMWARE_DIR/index.html
