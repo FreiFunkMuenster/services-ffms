5d5efb27672e1d7af8016413242af42ed97f487e
Thomas Hollstegge <thomas.hollstegge@zweitag.de>
2014-07-23 22:44:19 +0200

    Make overview page includable

--- generate_overview/generate_overview.sh
+++ generate_overview/generate_overview.sh
@@ -2,6 +2,7 @@
 
 FIRMWARE_DIR=/var/www/firmware
 TEMPLATE_DIR=$(dirname $0)
+TARGET_FILE=/var/www/wordpress/ressources/firmware.html
 
 STABLE_VERSION=$(awk '{print $2}' < $FIRMWARE_DIR/stable/sysupgrade/manifest | sort | uniq | tail -n1)
 BETA_VERSION=$(awk '{print $2}' < $FIRMWARE_DIR/beta/sysupgrade/manifest | sort | uniq | tail -n1)
@@ -10,4 +11,4 @@ export STABLE_VERSION;
 export BETA_VERSION;
 export EXPERIMENTAL_VERSION;
 
-sed -e "s/STABLE_VERSION/$STABLE_VERSION/g" -e "s/BETA_VERSION/$BETA_VERSION/g" -e "s/EXPERIMENTAL_VERSION/$EXPERIMENTAL_VERSION/g" $TEMPLATE_DIR/template.html > $FIRMWARE_DIR/index.html
+sed -e "s/STABLE_VERSION/$STABLE_VERSION/g" -e "s/BETA_VERSION/$BETA_VERSION/g" -e "s/EXPERIMENTAL_VERSION/$EXPERIMENTAL_VERSION/g" $TEMPLATE_DIR/template.html > $TARGET_FILE
