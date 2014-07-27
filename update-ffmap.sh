e3c537efa24f36b140dcd221124a74e2dbef53f5
Jan-Marten Brüggemann <fussel@fusselkate.org>
2014-07-27 06:39:53 +0200

    update-ffmap.sh gelöscht

--- update-ffmap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Shellscript zum Aufruf von Bat2Nodes mit entsprechenden
-# Parametern
-
-set -e
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-/usr/bin/python3 /var/ffmap-backend/bat2nodes.py -A -o -a /var/ffmap-backend/aliases_gateways.json -d /var/ffmap-d3/
