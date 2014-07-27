db5f537ba42cf937a92ce040b5ba0081a4f33d0f
Jan-Marten Brüggemann <fussel@fusselkate.org>
2014-07-27 06:41:17 +0200

    aliases-gateways hinzugefügt, script angepasst

--- ffmap/update-ffmap.sh
+++ ffmap/update-ffmap.sh
@@ -6,4 +6,4 @@
 set -e
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-/usr/bin/python3 /var/ffmap-backend/bat2nodes.py -A -o -a /var/ffmap-backend/aliases_gateways.json -d /var/ffmap-d3/
+/usr/bin/python3 /var/ffmap-backend/bat2nodes.py -A -o -a /var/services-ffms/ffmap/aliases_gateways.json -d /var/ffmap-d3/
