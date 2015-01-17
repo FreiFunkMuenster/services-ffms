a53234400e77d94f1e9107c926e7f8187c2ba1b1
Christian Elberfeld <elberfeld@web.de>
2015-01-17 22:06:15 +0100

    ^script angepasst für neuen server und neue upstream version

--- ffmap/update-ffmap.sh
+++ ffmap/update-ffmap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
-# Shellscript zum Aufruf von Bat2Nodes mit entsprechenden
-# Parametern
+# Shellscript zum Aufruf von Bat2Nodes mit entsprechenden Parametern
+# 
 
 set -e
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-/usr/bin/python3 /var/ffmap-backend/bat2nodes.py -A -o -a /var/services-ffms/ffmap/aliases_gateways.json -d /var/ffmap-d3/
+/usr/bin/python3 /var/ffmap-backend/bat2nodes.py -A -a /var/services-ffms/ffmap/aliases_gateways.json -d /var/www/map/ 
+
