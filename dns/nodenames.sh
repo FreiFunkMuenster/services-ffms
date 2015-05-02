5b8396afc70fcadb4eb3a3b57d4ac0c83afcd64c
Christian Elberfeld <elberfeld@web.de>
2015-05-03 01:23:25 +0200

    script zur erzeugung der dns daten aus alfred

---
+++ dns/nodenames.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+# 
+
+set -e
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Generate Bind DB with Node Names 
+python /var/gateway-ffms/nodenames.py > /var/tmp/db.nodes.ffms.new
+RETVAL=$?
+
+if [ $RETVAL -eq 0 ] 
+  then 
+
+  # compare md5 sums 
+  MD5OLD=`/var/tmp/db.nodes.ffms` 
+  MD5NEW=`/var/tmp/db.nodes.ffms.new` 
+  
+  if [ $MD5OLD -ne $MD5NEW ] 
+    then
+
+    # Copy generated output 
+    mv /var/tmp/db.nodes.ffms.new /var/tmp/db.nodes.ffms
+
+    # Reload Bind9
+    service bind9 reload
+  fi 
+  
+  # cleanup 
+  rm /var/tmp/db.nodes.ffms.new
+  
+fi 
+
