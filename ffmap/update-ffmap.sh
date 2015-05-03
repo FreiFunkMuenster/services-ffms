ee30618433299a45aa6848decb17aa1243ea05dd
Christian Elberfeld <elberfeld@web.de>
2015-05-03 17:42:54 +0200

    if vor mkdir

--- ffmap/update-ffmap.sh
+++ ffmap/update-ffmap.sh
@@ -6,14 +6,21 @@
 set -e
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-mkdir /var/ffmap-data
+if [ ! -f /var/ffmap-data ]
+  then 
+  mkdir /var/ffmap-data
+fi
+
 cd /var/ffmap-data
 
 # FFMap Backend ausführen, Daten werden in /var/www/map/rawdata geschrieben 
 /usr/bin/python3 /var/ffmap-backend/backend.py -d /var/ffmap-data -a /var/services-ffms/ffmap/aliases_gateways.json --vpn de:ad:be:ef:43:02 de:ad:be:ef:43:03 de:ad:be:ef:43:04 de:ad:be:ef:43:05 de:ad:be:ef:43:06 de:ad:be:ef:43:07 de:ad:be:ef:43:08 de:ad:be:ef:43:09
 
 # Aus der Nodes.json die Owner Informationen entfernen 
-mkdir /var/www/map/data
+if [ ! -f /var/www/map/data ]
+  then 
+  mkdir /var/www/map/data
+fi
 jq '.nodes = (.nodes | with_entries(del(.value.nodeinfo.owner)))' < /var/ffmap-data/nodes.json > /var/www/map/data/nodes.json
 
 # Restliche Daten kopieren 
