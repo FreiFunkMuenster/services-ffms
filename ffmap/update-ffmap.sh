9daf4b15dc21a28f8d78318f3ffd3e50988eebb3
Christian Elberfeld <elberfeld@web.de>
2015-05-03 17:10:03 +0200

    aliases und aupdate script für neue ffmap version aktualisiert

--- ffmap/update-ffmap.sh
+++ ffmap/update-ffmap.sh
@@ -6,5 +6,14 @@
 set -e
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-/usr/bin/python3 /var/ffmap-backend/bat2nodes.py -A -a /var/services-ffms/ffmap/aliases_gateways.json -d /var/www/map/ 
+cd /tmp
 
+# FFMap Backend ausführen, Daten werden in /var/www/map/rawdata geschrieben 
+/usr/bin/python3 /var/ffmap-backend/backend.py -d /var/www/map/rawdata/ -a /var/services-ffms/ffmap/aliases_gateway.json --vpn de:ad:be:ef:43:02 de:ad:be:ef:43:03 de:ad:be:ef:43:04 de:ad:be:ef:43:05 de:ad:be:ef:43:06 de:ad:be:ef:43:07 de:ad:be:ef:43:08 de:ad:be:ef:43:09
+
+# Aus der Nodes.json die Owner Informationen entfernen 
+jq '.nodes = (.nodes | with_entries(del(.value.nodeinfo.owner)))' < /var/www/map/rawdata/nodes.json > /var/www/map/build/data/nodes.json
+
+# Restliche Daten kopieren 
+cp /var/www/map/rawdata/graph.json /var/www/map/build/data/
+cp /var/www/map/rawdata/nodelist.json /var/www/map/build/data/
