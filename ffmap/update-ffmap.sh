39ab8adeb7f1fb5d039fefedf82d6e795733ccf5
Christian Elberfeld <elberfeld@web.de>
2015-05-03 17:21:00 +0200

    verzeichnisse umstrukturiert

--- ffmap/update-ffmap.sh
+++ ffmap/update-ffmap.sh
@@ -6,14 +6,16 @@
 set -e
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-cd /tmp
+mkdir /var/ffmap-data
+cd /var/ffmap-data
 
 # FFMap Backend ausführen, Daten werden in /var/www/map/rawdata geschrieben 
-/usr/bin/python3 /var/ffmap-backend/backend.py -d /var/www/map/rawdata/ -a /var/services-ffms/ffmap/aliases_gateways.json --vpn de:ad:be:ef:43:02 de:ad:be:ef:43:03 de:ad:be:ef:43:04 de:ad:be:ef:43:05 de:ad:be:ef:43:06 de:ad:be:ef:43:07 de:ad:be:ef:43:08 de:ad:be:ef:43:09
+/usr/bin/python3 /var/ffmap-backend/backend.py -d /var/ffmap-data -a /var/services-ffms/ffmap/aliases_gateways.json --vpn de:ad:be:ef:43:02 de:ad:be:ef:43:03 de:ad:be:ef:43:04 de:ad:be:ef:43:05 de:ad:be:ef:43:06 de:ad:be:ef:43:07 de:ad:be:ef:43:08 de:ad:be:ef:43:09
 
 # Aus der Nodes.json die Owner Informationen entfernen 
-jq '.nodes = (.nodes | with_entries(del(.value.nodeinfo.owner)))' < /var/www/map/rawdata/nodes.json > /var/www/map/build/data/nodes.json
+mkdir /var/www/map/data
+jq '.nodes = (.nodes | with_entries(del(.value.nodeinfo.owner)))' < /var/ffmap-data/nodes.json > /var/www/map/data/nodes.json
 
 # Restliche Daten kopieren 
-cp /var/www/map/rawdata/graph.json /var/www/map/build/data/
-cp /var/www/map/rawdata/nodelist.json /var/www/map/build/data/
+cp /var/ffmap-data/graph.json /var/www/map/data/
+cp /var/ffmap-data/nodelist.json /var/www/map/data/
