943b3255d2b7ea78d6cff8002af72b3ce5ae2859
Christian Elberfeld <elberfeld@web.de>
2015-10-10 13:09:14 +0200

    memory limit für graphite prozesse

--- graphite/docker-setup.sh
+++ graphite/docker-setup.sh
@@ -11,6 +11,6 @@ docker build -t graphite .
 
 # Setup and Rund Docker Container 
 # Double mounting of volumes is needed, otherwise no data is written by carbon-cache
-docker run --name=graphite --restart=always -d -p 127.0.0.1:8083:8000  -p 10.43.0.12:2003:2003 -p 10.43.0.12:2004:2004 -v /var/data/graphite:/var/lib/graphite/storage -v /var/data/graphite/whisper:/var/lib/graphite/storage/whisper graphite 
+docker run --name=graphite --restart=always -d -m 2g -p 127.0.0.1:8083:8000  -p 10.43.0.12:2003:2003 -p 10.43.0.12:2004:2004 -v /var/data/graphite:/var/lib/graphite/storage -v /var/data/graphite/whisper:/var/lib/graphite/storage/whisper graphite 
 
 
