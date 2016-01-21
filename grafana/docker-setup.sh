07ddb526c3c6d17413c9667125464a7a38ca5d28
Christian Elberfeld <elberfeld@connectiv.de>
2016-01-21 23:26:23 +0100

    grafana auf version 2.6.0 und korrektur datenpfad

--- grafana/docker-setup.sh
+++ grafana/docker-setup.sh
@@ -7,6 +7,6 @@ docker stop grafana
 docker rm grafana
 
 # Setup and Rund Docker Container 
-docker run --name=grafana --restart=always -d -p 127.0.0.1:8086:3000 -v /var/data/igrafana/:/var/lib/grafana -e "GF_SERVER_ROOT_URL=https://freifunk-muensterland.de/grafana/" -e "GF_USERS_ALLOW_SIGN_UP=false" -e "GF_USERS_ALLOW_ORG_CREATE=false" -e "GF_AUTH_ANONYMOUS_ENABLED=true" -e "GF_SESSION_PROVIDER=memory" -e "GF_ANALYTICS_REPORTING_ENABLED=false" grafana/grafana:2.0.2 
+docker run --name=grafana --restart=always -d -p 127.0.0.1:8086:3000 -v /var/data/grafana/:/var/lib/grafana -e "GF_SERVER_ROOT_URL=https://freifunk-muensterland.de/grafana/" -e "GF_USERS_ALLOW_SIGN_UP=false" -e "GF_USERS_ALLOW_ORG_CREATE=false" -e "GF_AUTH_ANONYMOUS_ENABLED=true" -e "GF_SESSION_PROVIDER=memory" -e "GF_ANALYTICS_REPORTING_ENABLED=false" grafana/grafana:2.6.0 
 
 
